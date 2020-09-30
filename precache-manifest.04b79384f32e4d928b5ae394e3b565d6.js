eb6aa0199bb87c0365005857b35a98b8f5d4fcc0
Zurab <zurab@candis.io>
2020-09-30 02:34:00 +0200

    Updates

---
+++ precache-manifest.04b79384f32e4d928b5ae394e3b565d6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9cfaec4b96334a91e31",
+    "url": "/generate-pdfs/static/css/main.2a7c012c.chunk.css"
+  },
+  {
+    "revision": "c9cfaec4b96334a91e31",
+    "url": "/generate-pdfs/static/js/main.c9cfaec4.chunk.js"
+  },
+  {
+    "revision": "36012467c9564747468e",
+    "url": "/generate-pdfs/static/js/1.36012467.chunk.js"
+  },
+  {
+    "revision": "5f9bcc21194ac259d66a",
+    "url": "/generate-pdfs/static/js/runtime~main.5f9bcc21.js"
+  },
+  {
+    "revision": "c89ef653554f9eee05f32cd875eee4c6",
+    "url": "/generate-pdfs/index.html"
+  }
+];
